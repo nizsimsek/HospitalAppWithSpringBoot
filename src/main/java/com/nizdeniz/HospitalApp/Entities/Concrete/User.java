f501f55c73544b416ba52387d67d8d1cde56ea37
nizsimsek <19720111036@ybu.edu.tr>
2021-03-17 17:20:48 +0300

    User Added

---
+++ src/main/java/com/nizdeniz/HospitalApp/Entities/Concrete/User.java
@@ -0,0 +1,51 @@
+package com.nizdeniz.HospitalApp.Entities.Concrete;
+
+import com.nizdeniz.HospitalApp.Entities.Abstract.IEntity;
+
+public class User implements IEntity {
+    private int Id;
+    private String Email;
+    private String PasswordHash;
+    private String PasswordSalt;
+    private int RoleId;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPasswordHash() {
+        return PasswordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        PasswordHash = passwordHash;
+    }
+
+    public String getPasswordSalt() {
+        return PasswordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        PasswordSalt = passwordSalt;
+    }
+
+    public int getRoleId() {
+        return RoleId;
+    }
+
+    public void setRoleId(int roleId) {
+        RoleId = roleId;
+    }
+}
