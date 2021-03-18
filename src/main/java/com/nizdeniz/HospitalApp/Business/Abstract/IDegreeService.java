91438f965e533ab1b7bf5100f71a71205a0d5b88
nizsimsek <19720111036@ybu.edu.tr>
2021-03-18 20:46:50 +0300

    Services Changed

--- src/main/java/com/nizdeniz/HospitalApp/Business/Abstract/IDegreeService.java
+++ src/main/java/com/nizdeniz/HospitalApp/Business/Abstract/IDegreeService.java
@@ -1,6 +1,17 @@
 package com.nizdeniz.HospitalApp.Business.Abstract;
 
-import com.nizdeniz.HospitalApp.DataAccess.Abstract.IDegreeDal;
+import com.nizdeniz.HospitalApp.Entities.Concrete.Degree;
 
-public interface IDegreeService extends IDegreeDal {
+import java.util.List;
+
+public interface IDegreeService {
+    List<Degree> GetAll();
+
+    void Add(Degree degree);
+
+    void Update(Degree degree);
+
+    void Delete(Degree degree);
+
+    Degree GetById(int id);
 }
